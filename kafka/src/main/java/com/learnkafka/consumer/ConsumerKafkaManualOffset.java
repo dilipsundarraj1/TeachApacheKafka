c112c95e8553e85c2162e67e6648f9581e24c219
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-03-26 18:40:42 -0500

    added the consumer offset code

--- kafka/src/main/java/com/learnkafka/consumer/ConsumerKafkaManualOffset.java
+++ kafka/src/main/java/com/learnkafka/consumer/ConsumerKafkaManualOffset.java
@@ -7,37 +7,39 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
-public class ConsumerKafka {
+public class ConsumerKafkaManualOffset {
 	
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		Properties properties=new Properties();
 		properties.put("bootstrap.servers", "localhost:9092");
 		properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.put("enable.auto.commit", false);
 		properties.put("group.id","test1");
-		
+
 		KafkaConsumer< String, String> consumer=new KafkaConsumer<String, String>(properties);
-		
-		
+
+
 		ArrayList<String> topics=new ArrayList<String>();
 		topics.add("my-fifth-topic");
-		
+
 		consumer.subscribe(topics); // You can subscribe to any number of topics.
-		
+
 		try {
-			
+
 			while(true){
-				
-				ConsumerRecords<String, String> records = consumer.poll(10);
-				
+
+				ConsumerRecords<String, String> records = consumer.poll(1000);
+
 				for(ConsumerRecord<String, String> record : records){
-					
+
 					System.out.println("Record read in KafkaConsumerApp : " +  record.toString());
-					
+
+					//consumer.commitSync();
 				}
 			}
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println("Inside exception loop : ");
